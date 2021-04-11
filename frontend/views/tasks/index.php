d0f948234edd07e4755bff340b0bd4b6d96ec6f0
Marat <shaimardanov@infomatika.ru>
2021-04-11 13:50:48 +0300

    fix-bugs: Добавляет data provider

--- frontend/views/tasks/index.php
+++ frontend/views/tasks/index.php
@@ -1,14 +1,15 @@
 <?
 
-use htmlacademy\helpers\SiteHelper;
-use yii\helpers\Url;
+use frontend\widgets\TasksPagination;
+
 use yii\widgets\ActiveForm;
 use yii\widgets\LinkPager;
 
-/** @var $tasks */
+
+/** @var $dataProvider */
+/** @var $pages */
 /** @var $categories */
 /** @var $model */
-/** @var $pages */
 
 $this->title = 'TaskForce | Список задач';
 ?>
@@ -16,33 +17,11 @@ $this->title = 'TaskForce | Список задач';
 <section class="new-task">
     <div class="new-task__wrapper">
         <h1>Новые задания</h1>
-        <? foreach ($tasks as $task): ?>
-            <div class="new-task__card">
-                <div class="new-task__title">
-                    <a href="<?= Url::to(["/tasks/" . $task['id']]) ?>" class="link-regular">
-                        <h2><?= $task['name'] ?></h2></a>
-                    <div class="links" style="display: flex;margin-left: -10px;">
-                        <? foreach ($task->categories as $category): ?>
-                            <a class="new-task__type link-regular" style="margin-left: 10px;"
-                               href="<?= Url::to(["/category/" . $category['slug']]) ?>">
-                                <p><?= $category['name'] ?></p>
-                            </a>
-                        <? endforeach; ?>
-                    </div>
-                </div>
-                <div
-                    class="new-task__icon new-task__icon--<?= SiteHelper::array_first($task->categories)['slug'] ?>"></div>
-                <p class="new-task_description">
-                    <?= $task['description'] ?>
-                </p>
-                <b class="new-task__price new-task__price--translation"><?= $task['price'] ?><b> ₽</b></b>
-                <p class="new-task__place"><?= $task['address'] ?></p>
-                <span class="new-task__time"><?= Yii::$app->formatter->asRelativeTime($task['date_add']) ?></span>
-            </div>
-        <? endforeach; ?>
+        <?= TasksPagination::widget(['dataProvider' => $dataProvider]) ?>
     </div>
     <div class="new-task__pagination">
-        <?= LinkPager::widget([
+        <?=
+        LinkPager::widget([
             'pagination' => $pages,
             'activePageCssClass' => 'pagination__item--current',
             'pageCssClass' => 'pagination__item',
@@ -53,7 +32,8 @@ $this->title = 'TaskForce | Список задач';
             'options' => [
                 'class' => 'new-task__pagination-list',
             ]
-        ]); ?>
+        ]);
+        ?>
     </div>
 </section>
 <section class="search-task">
